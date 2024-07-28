e210b420d4cba701bf2d3a9248efc2ad8f495dd7
xsyv <bugcodess@gmail.com>
2024-07-28 14:39:22 +0100

    Refactor main.js to update variable assignment

---
+++ Part 3/main.js
@@ -0,0 +1,16 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+
+// Variable that can be changed.
+var myName = "itooktime";
+
+// Variable that can be changed.
+myName = 8;
+
+// Variable that can be changed.
+let ourName = "itooktime";
+
+// Variable that cannot be changed.
+const pi = 3.14;
